b0f60f3d7747314e735df98a7fe0a84b7820110f
Anasch113 <anaschaudry2002@gmail.com>
2025-01-02 18:15:44 +0500

    commit

--- src/extras/storage.js
+++ src/extras/storage.js
@@ -31,3 +31,62 @@ for await (const chunk of stream) {
 // anas ch hf token: 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
 
 // uscriber hf token: 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
+
+
+
+
+
+
+
+
+
+
+    // const hfInference = async (e, transcript) => {
+    //     // console.log("transcript", transcript)
+
+
+
+    //     if (e.type === 'click') {
+    //         e.preventDefault()
+    //     }
+    //     stopSTT()
+
+    //     try {
+    //         const currentUserResponse = giveUserResponse();
+    //         console.log("Current user response:", transcript);
+
+    //         setIsProcessing(true);
+
+    //         // Add user message to the state
+    //         setMessages((prevMessages) => [
+    //             ...prevMessages,
+    //             { role: 'user', content: transcript || currentUserResponse },
+    //         ]);
+
+    //         // Clear input (if needed)
+    //         setUserResponse("");
+
+    //         // Get model response
+    //         const stream = await openai.chat.completions.create({
+    //             model: "tgi",
+    //             messages: [{ role: "user", content: transcript || currentUserResponse }],
+    //             max_tokens: 150,
+    //             stream: false,
+    //         });
+
+    //         const response = stream.choices[0]?.message?.content || "No response";
+
+    //         console.log("Model response:", response);
+
+    //         // Add model response to the state
+    //         setMessages((prevMessages) => [
+    //             ...prevMessages,
+    //             { role: 'assistant', content: response },
+    //         ]);
+    //         speakText(response)
+    //         setIsProcessing(false);
+    //     } catch (error) {
+    //         console.error("Error:", error);
+    //         setIsProcessing(false);
+    //     }
+    // };
